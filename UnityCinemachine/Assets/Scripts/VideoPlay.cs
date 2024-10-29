1a5c0f2802fe535de981465de92cfde6a2af7622
Kim-Sung-Gwon <sksmkim209@gmail.com>
2024-10-29 10:51:52 +0900

    시네머신을 활용한 여러가지 기법

---
+++ UnityCinemachine/Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlay : MonoBehaviour
+{
+    public VideoPlayer player;
+
+    void Start()
+    {
+        player.Play();
+    }
+
+    public void PlayVideo()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
